bab7547e89aba29fdce76ee2ca1fb43cf220f8b4
Pascal Loose <pascal.loose@me.com>
2020-06-03 16:45:36 +0100

    IWK and Grapher compatible

--- simple_touch_bluetooth/simple_touch_bluetooth.ino
+++ simple_touch_bluetooth/simple_touch_bluetooth.ino
@@ -1,72 +1,129 @@
+/*******************************************************************************
+
+ Bare Conductive HID keyboard demo
+ ---------------------------------
+
+ HID_Keyboard.ino - simple MPR121 touch detection to faked keyboard output
+
+ Bare Conductive code written by Stefan Dzisiewski-Smith and Peter Krige.
+
+ This work is licensed under a MIT license https://opensource.org/licenses/MIT
+
+ Copyright (c) 2020, Bare Conductive
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+
+*******************************************************************************/
+
+// touch includes
 #include <MPR121.h>
+#include <MPR121_Datastream.h>
 #include <Wire.h>
+
+// serial includes
 #include <SoftwareSerial.h>
 
-#define numElectrodes 12
+// touch constants
+const uint32_t BAUD_RATE = 115200;
+const uint8_t MPR121_ADDR = 0x5C;
+const uint8_t MPR121_INT = 4;
+
+// MPR121 datastream behaviour constants
+const bool MPR121_DATASTREAM_ENABLE = false;
+
+// serial instantiation
 SoftwareSerial newSerial(0, 1);
 
-void setup()
-{
-  newSerial.begin(9600);
-  
-  // 0x5C is the MPR121 I2C address on the Bare Touch Board
-  if(!MPR121.begin(0x5C)){ 
-    Serial.println("error setting up MPR121");  
-    switch(MPR121.getError()){
+// serial constants
+const uint32_t NEW_BAUD_RATE = 9600;
+
+void setup() {
+  if (MPR121_DATASTREAM_ENABLE) {
+    Serial.begin(BAUD_RATE);
+  } else {
+    newSerial.begin(NEW_BAUD_RATE);
+  }
+
+  if (!MPR121.begin(MPR121_ADDR)) {
+    Serial.println("error setting up MPR121");
+    switch (MPR121.getError()) {
       case NO_ERROR:
-        newSerial.println("no error");
-        break;  
+        Serial.println("no error");
+        break;
       case ADDRESS_UNKNOWN:
-        newSerial.println("incorrect address");
+        Serial.println("incorrect address");
         break;
       case READBACK_FAIL:
-        newSerial.println("readback failure");
+        Serial.println("readback failure");
         break;
       case OVERCURRENT_FLAG:
-        newSerial.println("overcurrent on REXT pin");
-        break;      
+        Serial.println("overcurrent on REXT pin");
+        break;
       case OUT_OF_RANGE:
-        newSerial.println("electrode out of range");
+        Serial.println("electrode out of range");
         break;
       case NOT_INITED:
-        newSerial.println("not initialised");
+        Serial.println("not initialised");
         break;
       default:
-        newSerial.println("unknown error");
-        break;      
+        Serial.println("unknown error");
+        break;
     }
-    while(1);
+    while (1);
   }
-  
-  // pin 4 is the MPR121 interrupt on the Bare Touch Board
-  MPR121.setInterruptPin(4);
 
-  // this is the touch threshold - setting it low makes it more like a proximity trigger
-  // default value is 40 for touch
-  MPR121.setTouchThreshold(40);
-  
-  // this is the release threshold - must ALWAYS be smaller than the touch threshold
-  // default value is 20 for touch
-  MPR121.setReleaseThreshold(20);  
+  MPR121.setInterruptPin(MPR121_INT);
 
-  // initial data update
-  MPR121.updateTouchData();
+  if (MPR121_DATASTREAM_ENABLE) {
+    MPR121.restoreSavedThresholds();
+    MPR121_Datastream.begin(&Serial);
+  } else {
+    MPR121.setTouchThreshold(40);
+    MPR121.setReleaseThreshold(20);
+  }
+
+  MPR121.setFFI(FFI_10);
+  MPR121.setSFI(SFI_10);
+  MPR121.setGlobalCDT(CDT_4US);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  MPR121.autoSetElectrodes();
+  digitalWrite(LED_BUILTIN, LOW);
 }
 
-void loop()
-{
-  if(MPR121.touchStatusChanged()){
-    MPR121.updateTouchData();
-    for(int i=0; i<numElectrodes; i++){
-      if(MPR121.isNewTouch(i)){
-        newSerial.print("electrode ");
-        newSerial.print(i, DEC);
-        newSerial.println(" was just touched");
-      } else if(MPR121.isNewRelease(i)){
-        newSerial.print("electrode ");
-        newSerial.print(i, DEC);
-        newSerial.println(" was just released"); 
-      }
-    } 
+void loop() {
+  MPR121.updateAll();
+
+  for (int i=0; i < 12; i++) {
+    if (MPR121.isNewTouch(i)) {
+      newSerial.print("electrode ");
+      newSerial.print(i, DEC);
+      newSerial.println(" was just touched");
+    } else if (MPR121.isNewRelease(i)) {
+      newSerial.print("electrode ");
+      newSerial.print(i, DEC);
+      newSerial.println(" was just released");
+    }
+  }
+
+  if (MPR121_DATASTREAM_ENABLE) {
+    MPR121_Datastream.update();
   }
 }
